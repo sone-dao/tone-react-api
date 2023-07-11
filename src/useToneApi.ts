7e4242f0636484d1f83bb9978ca0cf510e999780
Lon Beshiri <lonbeshiri@gmail.com>
2023-07-11 11:42:04 -0400

    refactor: tone object

--- src/useToneApi.ts
+++ src/useToneApi.ts
@@ -1,119 +1,83 @@
-interface ILyrics {
-  language: string
-  text: string
-  lines?: {
-    line: string
-    start: number
-    stop: number
-  }[]
-}
-
 export default function useToneApi() {
-  const api = 'https://api.tone.audio'
+  const api = 'https://tone.audio/api/v1'
   const auth = 'https://auth.tone.audio'
 
   const accessToken = sessionStorage.getItem('tone.access')
+  const sessionToken = localStorage.getItem('tone.session')
 
   const tone = {
-    artist: {
-      create: async (owners: string[], display: string, unique: string) =>
-        await post(api + '/artist', { owners, display, unique }),
-    },
-    artists: {
-      search: async (term: string) => await get(api + '/artists?q=' + term),
-    },
     auth: {
-      sendNonce: async (email: string) => await post(auth + '/email', {email}),
-      nonce: async (email: string, nonce: string, additionalData: any) =>
-        await post(auth + '/nonce', { email, nonce, additionalData }),
-      token: async (token: string) => await post(auth + '/token', { token }),
-    },
-    credits: {
-      search: async (term: string) => await get(api + '/credits?q=' + term),
-    },
-    label: {
-      create: async (owners: string[]) =>
-        await post(api + '/label', { owners }),
-    },
-    people: {
-      search: async (term: string) => await get(api + '/people?q=' + term),
-    },
-    person: {
-      create: async (person: string) => await post(api + '/person', { person }),
-    },
-    release: {
-      create: async (
-        owners: string[],
-        display: { release: string; artist: string } = {
-          release: '',
-          artist: '',
-        },
-        artists: string[] = [],
-        labels: string[] = [],
-        info: string = ''
-      ) =>
-        await post(api + '/release', {
-          owners,
-          display,
-          artists,
-          labels,
-          info,
-        }),
-      upload: {
-        art: async () => {},
+      email: {
+        attempt: async (email: string) =>
+          await get(auth + '/email?email=' + email),
+        login: async (email: string, nonce: string) =>
+          await get(auth + '/email/login?email=' + email + '&nonce=' + nonce),
       },
-    },
-    song: {
-      create: async (
-        releaseId: string,
-        artistIds: string[],
-        title: string = '',
-        description: string = '',
-        lyrics: ILyrics[] = [],
-        duration: number = 0
-      ) =>
-        await post(api + '/song', {
-          releaseId,
-          artistIds,
-          title,
-          description,
-          lyrics,
-          duration,
-        }),
-    },
-    tag: {
-      create: async (tag: string) => await post(api + '/tag', { tag }),
-    },
-    tags: {
-      search: async (term: string) => await get(api + '/tags?q=' + term),
-    },
-    user: {
-      get: async () => await get(api + '/user'),
     },
   }
 
   return tone
 
   async function post(url: string, data: any) {
-    return await fetch(url, {
+    const config = {
       method: 'POST',
       headers: {
         Authorization: `BEARER ${accessToken}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
-    })
+    }
+
+    const result = await fetch(url, config)
+      .then((response) => response.json())
+      .catch((error) => console.log(error))
+
+    if (result.status !== 401) return result
+
+    await genNewAccessToken()
+
+    return await fetch(url, config)
       .then((response) => response.json())
       .catch((error) => console.log(error))
   }
 
   async function get(url: string) {
-    return await fetch(url, {
+    const config = {
       headers: {
         Authorization: `BEARER ${accessToken}`,
       },
-    })
+    }
+
+    const result = await fetch(url, config)
       .then((response) => response.json())
       .catch((error) => console.log(error))
+
+    if (result.status !== 401) return result
+
+    await genNewAccessToken()
+
+    return await fetch(url, config)
+      .then((response) => response.json())
+      .catch((error) => console.log(error))
+  }
+
+  async function genNewAccessToken() {
+    const newAccessToken = await fetch(auth + '/token/renew', {
+      method: 'GET',
+      headers: {
+        Authorization: `BEARER ${sessionToken}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => data.token.access)
+
+    if (!newAccessToken) {
+      //Send user to login page, etc
+      localStorage.setItem('tone.session', '')
+      return console.log('Invalid session token.')
+    }
+
+    sessionStorage.setItem('tone.access', newAccessToken)
   }
 }
