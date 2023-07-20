93afc0cd2c8526c1cdffe2cce48fa929f1f98d31
Lon Beshiri <lonbeshiri@gmail.com>
2023-07-20 12:32:49 -0400

    fix: artwork content type

--- src/useToneApi.ts
+++ src/useToneApi.ts
@@ -138,6 +138,7 @@ export default function useToneApi() {
               headers: {
                 Authorization:
                   'BEARER ' + sessionStorage.getItem('tone.access'),
+                'Content-Type': 'multipart/form-data',
               },
               body: new FormData().append('file', file),
             }),
