93301709fcfc080be88ed6e40ab0f175ede8aa28
Lon Beshiri <lonbeshiri@gmail.com>
2023-08-19 19:46:31 -0400

    fix: fetch

--- src/useToneApi.ts
+++ src/useToneApi.ts
@@ -189,17 +189,15 @@ export default function useToneApi() {
       body: JSON.stringify(data),
     }
 
-    const result = await fetch(url, config).then((response) =>
-      response.ok ? response.json() : response.body
-    )
+    await fetch(url, config).then((response) => console.log(response))
 
-    if (result.status !== 401) return result
+    /*if (result.status !== 401) return result
 
     await genNewAccessToken()
 
     return await fetch(url, config).then((response) =>
       response.ok ? response.json() : response.body
-    )
+    )*/
   }
 
   async function genNewAccessToken() {
