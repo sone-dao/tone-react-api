941793f72f64bf1ddd68118c85a52473575bd494
Lon Beshiri <lonbeshiri@gmail.com>
2023-08-21 14:59:18 -0400

    fix: auth

--- src/useToneApi.ts
+++ src/useToneApi.ts
@@ -1,6 +1,5 @@
 'use client'
 
-import axios from 'axios'
 import { useRouter } from 'next/navigation'
 
 interface IToneReactApiAuthConfig {
@@ -190,7 +189,7 @@ export default function useToneApi() {
       body: JSON.stringify(data),
     }
 
-    const result = await axios(url, config)
+    const result = await fetch(url, config)
       .then((response) => console.log({ response }))
       .catch((error) => console.log({ error }))
 
